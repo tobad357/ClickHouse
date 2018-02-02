9d47ce6955442acf187b67ed17f7a7552e792785
Alexey Zatelepin <ztlpn@yandex-team.ru>
2018-02-02 19:02:43 +0300

    alter delete skeleton

--- dbms/src/Storages/StorageMergeTree.h
+++ dbms/src/Storages/StorageMergeTree.h
@@ -72,6 +72,8 @@ public:
 
     void alter(const AlterCommands & params, const String & database_name, const String & table_name, const Context & context) override;
 
+    void mutate(const MutationCommands & commands, const Context & context) override;
+
     bool supportsIndexForIn() const override { return true; }
     bool mayBenefitFromIndexForIn(const ASTPtr & left_in_operand) const override { return data.mayBenefitFromIndexForIn(left_in_operand); }
 
